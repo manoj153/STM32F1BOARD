adb4d09cfd0b5dac76d0d102bc06cd4acb179b60
Manojkumar <manojkumar99tech@gmail.com>
2017-11-15 16:27:09 +0800

    sample trigger to test, XOR

--- Src/main.c
+++ Src/main.c
@@ -425,7 +425,7 @@ void readChannel(uint8_t chWhat)
 void trigger(void)
 {
 	//CH6, Green ligth 
-	if (((sensors >> 0) & 1) && ((sensors >> 1) & 1 ) && ((sensors >> 2) & 1) && ((sensors >> 3) & 1))
+	if ((sensors >> 0 ^ 1) && (sensors >> 1 ^ 1 ) && (sensors >> 2 ^ 1) && (sensors >> 3 ^ 1))
 	{
 		//Turn ON Green LED for CH6
 		HAL_GPIO_WritePin(CH6G_6_GPIO_Port, CH6G_6_Pin, GPIO_PIN_SET);
@@ -439,7 +439,7 @@ void trigger(void)
 	{
 		HAL_GPIO_WritePin(CH6G_6_GPIO_Port, CH6G_6_Pin, GPIO_PIN_RESET);
 		//Each individual red led trigger CH6-1 -> CH1-1
-		if(~(sensors >> 0) & 1)
+		if((sensors >> 0) & 1)
 		{
 			HAL_GPIO_WritePin(CH6R_4_GPIO_Port, CH6R_4_Pin, GPIO_PIN_SET);
 		}
@@ -447,7 +447,7 @@ void trigger(void)
 		{
 			HAL_GPIO_WritePin(CH6R_4_GPIO_Port, CH6R_4_Pin, GPIO_PIN_RESET);
 		}
-		if(~(sensors >> 1) & 1)
+		if((sensors >> 1) & 1)
 		{
 			HAL_GPIO_WritePin(CH6R_3_GPIO_Port, CH6R_3_Pin, GPIO_PIN_SET);
 		}
@@ -455,7 +455,7 @@ void trigger(void)
 		{
 			HAL_GPIO_WritePin(CH6R_3_GPIO_Port, CH6R_3_Pin, GPIO_PIN_RESET);
 		}
-		if(~(sensors >> 2) & 1)
+		if((sensors >> 2) & 1)
 		{
 			HAL_GPIO_WritePin(CH6R_2_GPIO_Port, CH6R_2_Pin, GPIO_PIN_SET);
 		}
@@ -463,7 +463,7 @@ void trigger(void)
 		{
 			HAL_GPIO_WritePin(CH6R_2_GPIO_Port, CH6R_2_Pin, GPIO_PIN_RESET);
 		}
-		if(~(sensors >> 3) & 1)
+		if((sensors >> 3) & 1)
 		{
 			HAL_GPIO_WritePin(CH6R_1_GPIO_Port, CH6R_1_Pin, GPIO_PIN_SET);
 		}
